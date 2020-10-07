565cf69f4124098aad8226ff532c1ef65bb4a268
Alan Verbner <alan.verbner@iohk.io>
2020-10-07 18:50:51 -0300

    fix: example is not working with the latest rosetta api changed

--- examples/send-transaction-example.ts
+++ examples/send-transaction-example.ts
@@ -2,91 +2,120 @@
 /* eslint-disable camelcase */
 /* eslint-disable new-cap */
 /* eslint-disable no-console */
-import delay from 'delay';
-import * as NaCl from 'tweetnacl';
-import axios from 'axios';
-import { TransactionHash } from '@emurgo/cardano-serialization-lib-nodejs';
+import delay from "delay";
+import * as NaCl from "tweetnacl";
+import axios from "axios";
 
 const logger = console;
 
 const httpClient = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: "http://localhost:8080",
 });
 
 const PRIVATE_KEY =
-  '41d9523b87b9bd89a4d07c9b957ae68a7472d8145d7956a692df1a8ad91957a2c117d9dd874447f47306f50a650f1e08bf4bec2cfcb2af91660f23f2db912977';
+  "41d9523b87b9bd89a4d07c9b957ae68a7472d8145d7956a692df1a8ad91957a2c117d9dd874447f47306f50a650f1e08bf4bec2cfcb2af91660f23f2db912977";
 const SEND_FUNDS_ADDRESS =
-  'addr1qqr585tvlc7ylnqvz8pyqwauzrdu0mxag3m7q56grgmgu7sxu2hyfhlkwuxupa9d5085eunq2qywy7hvmvej456flknsug829n';
+  "addr1qqr585tvlc7ylnqvz8pyqwauzrdu0mxag3m7q56grgmgu7sxu2hyfhlkwuxupa9d5085eunq2qywy7hvmvej456flknsug829n";
 
 // FIXME: look for this using the service
 const network_identifier = {
-  blockchain: 'cardano',
-  network: 'testnet'
+  blockchain: "cardano",
+  network: "testnet",
 };
 
 const generateKeys = (secretKey?: string) =>
-  secretKey ? NaCl.sign.keyPair.fromSecretKey(Buffer.from(secretKey, 'hex')) : NaCl.sign.keyPair();
+  secretKey
+    ? NaCl.sign.keyPair.fromSecretKey(Buffer.from(secretKey, "hex"))
+    : NaCl.sign.keyPair();
 
 const constructionDerive = async (publicKey: string): Promise<string> => {
-  logger.info(`[constructionDerive] Fetching an address for pub key ${publicKey}`);
-  const response = await httpClient.post('/construction/derive', {
+  logger.info(
+    `[constructionDerive] Fetching an address for pub key ${publicKey}`
+  );
+  const response = await httpClient.post("/construction/derive", {
     network_identifier,
     public_key: {
       hex_bytes: publicKey,
-      curve_type: 'edwards25519'
+      curve_type: "edwards25519",
     },
-    metadata: {}
+    metadata: {},
   });
   const address = response.data.address;
   logger.debug(`[constructionDerive] Retrieved address ${address}`);
   return address;
 };
 
-const waitForBalanceToBe = async (address: string, cond: (param: any) => boolean) => {
+const waitForBalanceToBe = async (
+  address: string,
+  cond: (param: any) => boolean
+) => {
   let fetchAccountBalance;
   do {
-    const response = await httpClient.post('/account/balance', {
+    const response = await httpClient.post("/account/balance", {
       network_identifier,
       account_identifier: {
-        address
-      }
+        address,
+      },
     });
     if (cond(response.data)) {
       const [balance] = response.data.balances;
-      logger.info(`[waitForBalanceToBe] Funds found! ${balance.value} ${balance.currency.symbol}`);
+      logger.info(
+        `[waitForBalanceToBe] Funds found! ${balance.value} ${balance.currency.symbol}`
+      );
       fetchAccountBalance = response.data;
     } else {
-      logger.debug('[waitForBalanceToBe] Condition not met, waiting for a few seconds.');
+      logger.debug(
+        "[waitForBalanceToBe] Condition not met, waiting for a few seconds."
+      );
       await delay(30 * 1000);
     }
   } while (!fetchAccountBalance);
   return fetchAccountBalance;
 };
 
-const constructionMetadata = async (relative_ttl: number) => {
-  const response = await httpClient.post('/construction/metadata', { network_identifier, options: { relative_ttl } });
+const constructionPreprocess = async (
+  operations: any,
+  relative_ttl: number
+) => {
+  const response = await httpClient.post("/construction/preprocess", {
+    network_identifier,
+    operations,
+    metadata: { relative_ttl },
+  });
+  return response.data.options;
+};
+
+const constructionMetadata = async (options: any) => {
+  const response = await httpClient.post("/construction/metadata", {
+    network_identifier,
+    options,
+  });
   return response.data.metadata;
 };
 
-const buildOperation = (unspents: any, metadata: any, address: string, destination: string) => {
+const buildOperation = (
+  unspents: any,
+  address: string,
+  destination: string
+) => {
   const inputs = unspents.coins.map((coin: any, index: number) => {
     const operation = {
       operation_identifier: {
         index,
-        network_index: 0
+        network_index: 0,
       },
       related_operations: [],
-      type: 'transfer',
-      status: 'success',
+      type: "transfer",
+      status: "success",
       account: {
         address,
-        metadata: {}
+        metadata: {},
       },
       amount: coin.amount,
       coin_change: {
         coin_identifier: coin.coin_identifier,
-        coin_action: 'coin_created'
-      }
+        coin_action: "coin_created",
+      },
     };
     operation.amount.value = `-${operation.amount.value}`;
     return operation;
@@ -98,34 +127,33 @@ const buildOperation = (unspents: any, metadata: any, address: string, destinati
     {
       operation_identifier: {
         index: inputs.length,
-        network_index: 0
+        network_index: 0,
       },
       related_operations: [],
-      type: 'transfer',
-      status: 'success',
+      type: "transfer",
+      status: "success",
       account: {
         address: destination,
-        metadata: {}
+        metadata: {},
       },
       amount: {
         value: outputAmount.toString(),
         currency: {
-          symbol: 'ADA',
-          decimals: 6
+          symbol: "ADA",
+          decimals: 6,
         },
-        metadata: {}
-      }
-    }
+        metadata: {},
+      },
+    },
   ];
   return {
     network_identifier,
     operations: inputs.concat(outputs),
-    metadata
   };
 };
 
-const constructionPayloads = async (operations: any) => {
-  const response = await httpClient.post('/construction/payloads', operations);
+const constructionPayloads = async (payload: any) => {
+  const response = await httpClient.post("/construction/payloads", payload);
   return response.data;
 };
 
@@ -133,48 +161,71 @@ const signPayloads = (payloads: any, keys: NaCl.SignKeyPair) =>
   payloads.map((signing_payload: any) => ({
     signing_payload,
     public_key: {
-      hex_bytes: Buffer.from(keys.publicKey).toString('hex'),
-      curve_type: 'edwards25519'
+      hex_bytes: Buffer.from(keys.publicKey).toString("hex"),
+      curve_type: "edwards25519",
     },
-    signature_type: 'ed25519',
-    hex_bytes: Buffer.from(NaCl.sign.detached(Buffer.from(signing_payload.hex_bytes, 'hex'), keys.secretKey)).toString(
-      'hex'
-    )
+    signature_type: "ed25519",
+    hex_bytes: Buffer.from(
+      NaCl.sign.detached(
+        Buffer.from(signing_payload.hex_bytes, "hex"),
+        keys.secretKey
+      )
+    ).toString("hex"),
   }));
 
-const constructionCombine = async (unsigned_transaction: any, signatures: any) => {
-  const response = await httpClient.post('/construction/combine', {
+const constructionCombine = async (
+  unsigned_transaction: any,
+  signatures: any
+) => {
+  const response = await httpClient.post("/construction/combine", {
     network_identifier,
     unsigned_transaction,
-    signatures
+    signatures,
   });
   return response.data;
 };
 
 const constructionSubmit = async (signed_transaction: any) => {
-  const response = await httpClient.post('/construction/submit', {
+  const response = await httpClient.post("/construction/submit", {
     network_identifier,
-    signed_transaction
+    signed_transaction,
   });
   return response.data;
 };
 
 const doRun = async (): Promise<void> => {
   const keys = generateKeys(PRIVATE_KEY);
-  logger.info(`[doRun] secretKey ${Buffer.from(keys.secretKey).toString('hex')}`);
-  const address = await constructionDerive(Buffer.from(keys.publicKey).toString('hex'));
-  const unspents = await waitForBalanceToBe(address, response => response.coins.length !== 0);
-  const metadata = await constructionMetadata(1000);
-  const operations = buildOperation(unspents, metadata, address, SEND_FUNDS_ADDRESS);
-  const payloads = await constructionPayloads(operations);
+  logger.info(
+    `[doRun] secretKey ${Buffer.from(keys.secretKey).toString("hex")}`
+  );
+  const address = await constructionDerive(
+    Buffer.from(keys.publicKey).toString("hex")
+  );
+  const unspents = await waitForBalanceToBe(
+    address,
+    (response) => response.coins.length !== 0
+  );
+  const builtOperations = buildOperation(unspents, address, SEND_FUNDS_ADDRESS);
+  const preprocess = await constructionPreprocess(builtOperations.operations, 1000);
+  const metadata = await constructionMetadata(preprocess);
+  const payloads = await constructionPayloads({
+    network_identifier,
+    operations: builtOperations.operations,
+    metadata,
+  });
   const signatures = signPayloads(payloads.payloads, keys);
-  const combined = await constructionCombine(payloads.unsigned_transaction, signatures);
+  const combined = await constructionCombine(
+    payloads.unsigned_transaction,
+    signatures
+  );
   logger.info(`[doRun] signed transaction is ${combined.signed_transaction}`);
   const hashResponse = await constructionSubmit(combined.signed_transaction);
-  logger.info(`[doRun] transaction with hash ${hashResponse.transaction_identifier.hash} sent`);
-  await waitForBalanceToBe(address, response => response.coins.length === 0);
+  logger.info(
+    `[doRun] transaction with hash ${hashResponse.transaction_identifier.hash} sent`
+  );
+  await waitForBalanceToBe(address, (response) => response.coins.length === 0);
 };
 
 doRun()
-  .then(() => logger.info('Send Transaction finished'))
+  .then(() => logger.info("Send Transaction finished"))
   .catch(console.error);
