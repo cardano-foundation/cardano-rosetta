53152d68cd4525db96caa738fbbd9cabb71261eb
Tomás Bustamante <tbustamante@atixlabs.com>
2020-11-06 02:06:13 -0300

    test: construction derive with invalid staking key
    Refs #198

--- cardano-rosetta-server/test/e2e/construction/construction-derive-api.test.ts
+++ cardano-rosetta-server/test/e2e/construction/construction-derive-api.test.ts
@@ -7,14 +7,53 @@ import { setupDatabase, setupServer, testInvalidNetworkParameters } from '../uti
 const CONSTRUCTION_DERIVE_ENDPOINT = '/construction/derive';
 const INVALID_PUBLIC_KEY_FORMAT = 'Invalid public key format';
 
+type PublicKey = {
+  hex_bytes: string;
+  curve_type: string;
+};
+
+type DeriveMetadata = {
+  staking_credential?: PublicKey;
+  address_type?: string;
+};
+
+const generateMetadata = (addressType?: string, stakingKey?: string, curveType?: string): DeriveMetadata => {
+  const metadata: DeriveMetadata = {};
+  if (addressType) metadata.address_type = addressType;
+  if (stakingKey)
+    metadata.staking_credential = {
+      hex_bytes: stakingKey,
+      curve_type: curveType || 'edwards25519'
+    };
+  return metadata;
+};
+
 type GeneratePayloadInput = {
   blockchain: string;
   network: string;
   key?: string;
   curveType?: string;
+  type?: string;
+  stakingKey?: string;
 };
 
-const generatePayload = ({ blockchain, network, key, curveType }: GeneratePayloadInput) => ({
+type DerivePayload = {
+  network_identifier: {
+    blockchain: string;
+    network: string;
+  };
+  public_key: PublicKey;
+  metadata?: DeriveMetadata;
+};
+
+const generatePayload = ({
+  blockchain,
+  network,
+  key,
+  curveType,
+  type,
+  stakingKey
+}: GeneratePayloadInput): DerivePayload => ({
   network_identifier: {
     blockchain,
     network
@@ -22,7 +61,8 @@ const generatePayload = ({ blockchain, network, key, curveType }: GeneratePayloa
   public_key: {
     hex_bytes: key || '1B400D60AAF34EAF6DCBAB9BBA46001A23497886CF11066F7846933D30E5AD3F',
     curve_type: curveType || 'edwards25519'
-  }
+  },
+  metadata: generateMetadata(type, stakingKey, curveType)
 });
 
 describe(CONSTRUCTION_DERIVE_ENDPOINT, () => {
@@ -102,6 +142,21 @@ describe(CONSTRUCTION_DERIVE_ENDPOINT, () => {
         blockchain: 'cardano',
         network: 'mainnet',
         key: '1B400D60AAF34EAF6DCBAB9BBA46001A23497886CF11066F7846933D30E5AD3F__'
+      })
+    });
+    expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+    expect(response.json()).toEqual({ code: 4007, message: INVALID_PUBLIC_KEY_FORMAT, retriable: false });
+  });
+
+  test('Should return an error when the staking key has an invalid format', async () => {
+    const response = await server.inject({
+      method: 'post',
+      url: CONSTRUCTION_DERIVE_ENDPOINT,
+      payload: generatePayload({
+        blockchain: 'cardano',
+        network: 'mainnet',
+        type: 'Enterprise',
+        stakingKey: '1B400D60AAF34EAF6DCBAB9BBA46001A23497886CF11066F7846933D30E5AD3F__'
       })
     });
     expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
