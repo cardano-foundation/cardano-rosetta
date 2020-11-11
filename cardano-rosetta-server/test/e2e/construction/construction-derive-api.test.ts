0d8250a8b7c82791c2ccca9c4af1297d9059571d
Tomás Bustamante <bustamantejt@gmail.com>
2020-11-11 13:42:15 -0300

    feat: construction derive staking support (#207)
    * feat: add staking_credential and address_type in ConstructionDeriveRequest
    
    * refactor: move staking_credential and address_type into metadata of ConstructionDeriveRequest
    
    * feat: add invalid address type error
    
    * feat: add metadata validation in construction derive controller
    
    * refactor: generatePayload parameters in construction derive tests
    
    * test: construction derive with invalid staking key
    
    * refactor: exclude enum for address type in openApi
    
    * test: construction derive with invalid address type
    
    * test: construction derive for Base and Reward addresses
    
    * feat: construction derive Base and Reward address support
    
    * test: construction derive validation for staking key size
    
    * test: construction derive reward address correct bech32 address
    
    * fix: correct address prefix for reward address creation
    
    * refactor: move UTxOAddressTypes enum to constants file
    
    * refactor: rename UTxOAddressType and make Enterprise default value
    
    * refactor: add comment for staking address prefix
    
    * test: add comments to construction derive tests
    
    * refactor: use proper error for invalid staking key
    
    * test: construction derive error when no staking key for base address
    
    * feat: missing staking key validation for base address generation
    
    * refactor: rename AddressType
    
    * refactor: address type description in openApi
    
    Refs #198

--- cardano-rosetta-server/test/e2e/construction/construction-derive-api.test.ts
+++ cardano-rosetta-server/test/e2e/construction/construction-derive-api.test.ts
@@ -6,8 +6,57 @@ import { setupDatabase, setupServer, testInvalidNetworkParameters } from '../uti
 
 const CONSTRUCTION_DERIVE_ENDPOINT = '/construction/derive';
 const INVALID_PUBLIC_KEY_FORMAT = 'Invalid public key format';
+const INVALID_STAKING_KEY_FORMAT = 'Invalid staking key format';
+const STAKING_KEY_MISSING = 'Staking key is required for this type of address';
+const INVALID_ADDRESS_TYPE = 'Provided address type is invalid';
 
-const generatePayload = (blockchain: string, network: string, key?: string, curveType?: string) => ({
+type PublicKey = {
+  hex_bytes: string;
+  curve_type: string;
+};
+
+type DeriveMetadata = {
+  staking_credential?: PublicKey;
+  address_type?: string;
+};
+
+const generateMetadata = (addressType?: string, stakingKey?: string, curveType?: string): DeriveMetadata => {
+  const metadata: DeriveMetadata = {};
+  if (addressType) metadata.address_type = addressType;
+  if (stakingKey)
+    metadata.staking_credential = {
+      hex_bytes: stakingKey,
+      curve_type: curveType || 'edwards25519'
+    };
+  return metadata;
+};
+
+type GeneratePayloadInput = {
+  blockchain: string;
+  network: string;
+  key?: string;
+  curveType?: string;
+  type?: string;
+  stakingKey?: string;
+};
+
+type DerivePayload = {
+  network_identifier: {
+    blockchain: string;
+    network: string;
+  };
+  public_key: PublicKey;
+  metadata?: DeriveMetadata;
+};
+
+const generatePayload = ({
+  blockchain,
+  network,
+  key,
+  curveType,
+  type,
+  stakingKey
+}: GeneratePayloadInput): DerivePayload => ({
   network_identifier: {
     blockchain,
     network
@@ -15,7 +64,8 @@ const generatePayload = (blockchain: string, network: string, key?: string, curv
   public_key: {
     hex_bytes: key || '1B400D60AAF34EAF6DCBAB9BBA46001A23497886CF11066F7846933D30E5AD3F',
     curve_type: curveType || 'edwards25519'
-  }
+  },
+  metadata: generateMetadata(type, stakingKey, curveType)
 });
 
 describe(CONSTRUCTION_DERIVE_ENDPOINT, () => {
@@ -36,7 +86,7 @@ describe(CONSTRUCTION_DERIVE_ENDPOINT, () => {
     const response = await server.inject({
       method: 'post',
       url: CONSTRUCTION_DERIVE_ENDPOINT,
-      payload: generatePayload('cardano', 'mainnet')
+      payload: generatePayload({ blockchain: 'cardano', network: 'mainnet' })
     });
     expect(response.statusCode).toEqual(StatusCodes.OK);
     expect(response.json().address).toEqual('addr1vxa5pudxg77g3sdaddecmw8tvc6hmynywn49lltt4fmvn7cpnkcpx');
@@ -46,7 +96,7 @@ describe(CONSTRUCTION_DERIVE_ENDPOINT, () => {
     const response = await server.inject({
       method: 'post',
       url: CONSTRUCTION_DERIVE_ENDPOINT,
-      payload: generatePayload('cardano', 'mainnet', 'smallPublicKey')
+      payload: generatePayload({ blockchain: 'cardano', network: 'mainnet', key: 'smallPublicKey' })
     });
     expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
     expect(response.json()).toEqual({ code: 4007, message: INVALID_PUBLIC_KEY_FORMAT, retriable: false });
@@ -56,11 +106,11 @@ describe(CONSTRUCTION_DERIVE_ENDPOINT, () => {
     const response = await server.inject({
       method: 'post',
       url: CONSTRUCTION_DERIVE_ENDPOINT,
-      payload: generatePayload(
-        'cardano',
-        'mainnet',
-        'ThisIsABiggerPublicKeyForTestingPurposesThisIsABiggerPublicKeyForTestingPurposes'
-      )
+      payload: generatePayload({
+        blockchain: 'cardano',
+        network: 'mainnet',
+        key: 'ThisIsABiggerPublicKeyForTestingPurposesThisIsABiggerPublicKeyForTestingPurposes'
+      })
     });
     expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
     expect(response.json()).toEqual({ code: 4007, message: INVALID_PUBLIC_KEY_FORMAT, retriable: false });
@@ -68,7 +118,7 @@ describe(CONSTRUCTION_DERIVE_ENDPOINT, () => {
 
   testInvalidNetworkParameters(
     CONSTRUCTION_DERIVE_ENDPOINT,
-    (blockchain, network) => generatePayload(blockchain, network),
+    (blockchain, network) => generatePayload({ blockchain, network }),
     () => server
   );
 
@@ -76,12 +126,12 @@ describe(CONSTRUCTION_DERIVE_ENDPOINT, () => {
     const response = await server.inject({
       method: 'post',
       url: CONSTRUCTION_DERIVE_ENDPOINT,
-      payload: generatePayload(
-        'cardano',
-        'mainnet',
-        '1B400D60AAF34EAF6DCBAB9BBA46001A23497886CF11066F7846933D30E5AD3F',
-        'secp256k1'
-      )
+      payload: generatePayload({
+        blockchain: 'cardano',
+        network: 'mainnet',
+        key: '1B400D60AAF34EAF6DCBAB9BBA46001A23497886CF11066F7846933D30E5AD3F',
+        curveType: 'secp256k1'
+      })
     });
     expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
     expect(response.json()).toEqual({ code: 4007, message: INVALID_PUBLIC_KEY_FORMAT, retriable: false });
@@ -91,13 +141,120 @@ describe(CONSTRUCTION_DERIVE_ENDPOINT, () => {
     const response = await server.inject({
       method: 'post',
       url: CONSTRUCTION_DERIVE_ENDPOINT,
-      payload: generatePayload(
-        'cardano',
-        'mainnet',
-        '1B400D60AAF34EAF6DCBAB9BBA46001A23497886CF11066F7846933D30E5AD3F__.'
-      )
+      payload: generatePayload({
+        blockchain: 'cardano',
+        network: 'mainnet',
+        key: '1B400D60AAF34EAF6DCBAB9BBA46001A23497886CF11066F7846933D30E5AD3F__'
+      })
     });
     expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
     expect(response.json()).toEqual({ code: 4007, message: INVALID_PUBLIC_KEY_FORMAT, retriable: false });
+  });
+
+  test('Should return an error when the staking key has an invalid format', async () => {
+    const response = await server.inject({
+      method: 'post',
+      url: CONSTRUCTION_DERIVE_ENDPOINT,
+      payload: generatePayload({
+        blockchain: 'cardano',
+        network: 'mainnet',
+        type: 'Enterprise',
+        stakingKey: '1B400D60AAF34EAF6DCBAB9BBA46001A23497886CF11066F7846933D30E5AD3F__'
+      })
+    });
+    expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+    expect(response.json()).toEqual({ code: 4017, message: INVALID_STAKING_KEY_FORMAT, retriable: false });
+  });
+
+  test('Should return an error when the address type has an invalid value', async () => {
+    const response = await server.inject({
+      method: 'post',
+      url: CONSTRUCTION_DERIVE_ENDPOINT,
+      payload: generatePayload({
+        blockchain: 'cardano',
+        network: 'mainnet',
+        type: 'Invalid',
+        stakingKey: '1B400D60AAF34EAF6DCBAB9BBA46001A23497886CF11066F7846933D30E5AD3F'
+      })
+    });
+    expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+    expect(response.json()).toEqual({ code: 4016, message: INVALID_ADDRESS_TYPE, retriable: true });
+  });
+
+  // eslint-disable-next-line max-len
+  test('Should return the Base address corresponding to the public key, staking key and mainnet network when providing valid keys', async () => {
+    const response = await server.inject({
+      method: 'post',
+      url: CONSTRUCTION_DERIVE_ENDPOINT,
+      payload: generatePayload({
+        blockchain: 'cardano',
+        network: 'mainnet',
+        type: 'Base',
+        key: '159abeeecdf167ccc0ea60b30f9522154a0d74161aeb159fb43b6b0695f057b3',
+        stakingKey: '964774728c8306a42252adbfb07ccd6ef42399f427ade25a5933ce190c5a8760'
+      })
+    });
+    expect(response.statusCode).toEqual(StatusCodes.OK);
+    // Address generated locally with following command (using private keys attached to issue #198):
+    // cat addr.prv | ./cardano-address key public | ./cardano-address address payment  --network-tag 1 | ./cardano-address address delegation $(cat stake.prv | ./cardano-address key public)
+    expect(response.json().address).toEqual(
+      'addr1q9dhy809valxaer3nlvg2h5nudd62pxp6lu0cs36zczhfr98y6pah6lvppk8xft57nef6yexqh6rr204yemcmm3emhzsgg4fg0'
+    );
+  });
+
+  test('Should return an error when the address type is Base but no staking credentials are provided', async () => {
+    const response = await server.inject({
+      method: 'post',
+      url: CONSTRUCTION_DERIVE_ENDPOINT,
+      payload: generatePayload({
+        blockchain: 'cardano',
+        network: 'mainnet',
+        type: 'Base',
+        key: '159abeeecdf167ccc0ea60b30f9522154a0d74161aeb159fb43b6b0695f057b3'
+      })
+    });
+    expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+    expect(response.json()).toEqual({ code: 4018, message: STAKING_KEY_MISSING, retriable: false });
+  });
+
+  // eslint-disable-next-line max-len
+  test('Should return the Reward address corresponding to the public key and mainnet network when providing a valid public key', async () => {
+    const response = await server.inject({
+      method: 'post',
+      url: CONSTRUCTION_DERIVE_ENDPOINT,
+      payload: generatePayload({
+        blockchain: 'cardano',
+        network: 'mainnet',
+        type: 'Reward',
+        key: '964774728c8306a42252adbfb07ccd6ef42399f427ade25a5933ce190c5a8760'
+      })
+    });
+    expect(response.statusCode).toEqual(StatusCodes.OK);
+    // https://cardanoscan.io/address/015b721de5677e6ee4719fd8855e93e35ba504c1d7f8fc423a1605748ca72683dbebec086c732574f4f29d132605f431a9f526778dee39ddc5
+    expect(response.json().address).toEqual('stake1uxnjdq7ma0kqsmrny460fu5azvnqtap3486jvaudacuam3g3yc4nu');
+  });
+
+  test('Should return an error when the staking key has a lower length than 32', async () => {
+    const response = await server.inject({
+      method: 'post',
+      url: CONSTRUCTION_DERIVE_ENDPOINT,
+      payload: generatePayload({ blockchain: 'cardano', network: 'mainnet', stakingKey: 'smallPublicKey' })
+    });
+    expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+    expect(response.json()).toEqual({ code: 4017, message: INVALID_STAKING_KEY_FORMAT, retriable: false });
+  });
+
+  test('Should return an error when the staking key has a bigger length than 32', async () => {
+    const response = await server.inject({
+      method: 'post',
+      url: CONSTRUCTION_DERIVE_ENDPOINT,
+      payload: generatePayload({
+        blockchain: 'cardano',
+        network: 'mainnet',
+        stakingKey: 'ThisIsABiggerPublicKeyForTestingPurposesThisIsABiggerPublicKeyForTestingPurposes'
+      })
+    });
+    expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+    expect(response.json()).toEqual({ code: 4017, message: INVALID_STAKING_KEY_FORMAT, retriable: false });
   });
 });
--- cardano-rosetta-server/test/unit/services/cardano-services.test.ts
+++ cardano-rosetta-server/test/unit/services/cardano-services.test.ts
@@ -1,4 +1,4 @@
-import configure, { AddressType, CardanoService } from '../../../src/server/services/cardano-services';
+import configure, { EraAddressType, CardanoService } from '../../../src/server/services/cardano-services';
 
 describe('Cardano Service', () => {
   let cardanoService: CardanoService;
@@ -9,33 +9,33 @@ describe('Cardano Service', () => {
 
   describe('Address type detection', () => {
     it('Should detect a valid bech32 Shelley mainnet address', () => {
-      const addressType = cardanoService.getAddressType(
+      const addressType = cardanoService.getEraAddressType(
         'addr1q9ccruvttlfsqwu47ndmapxmk5xa8cc9ngsgj90290tfpysc6gcpmq6ejwgewr49ja0kghws4fdy9t2zecvd7zwqrheqjze0c7'
       );
-      expect(addressType).toBe(AddressType.Shelley);
+      expect(addressType).toBe(EraAddressType.Shelley);
     });
 
     it('Should properly detect a valid bech32 Shelley testnet address', () => {
-      const addressType = cardanoService.getAddressType(
+      const addressType = cardanoService.getEraAddressType(
         'addr_test1vru64wlzn85v7fecg0mz33lh00wlggqtquvzzuhf6vusyes32jz9w'
       );
-      expect(addressType).toBe(AddressType.Shelley);
+      expect(addressType).toBe(EraAddressType.Shelley);
     });
 
     it('Should detect a valid Byron address', () => {
-      const addressType = cardanoService.getAddressType(
+      const addressType = cardanoService.getEraAddressType(
         'DdzFFzCqrht9fvu17fiXwiuP82kKEhiGsDByRE7PWfMktrd8Jc1jWqKxubpz21mWjUMh8bWsKuP5JUF9CgUefyABDBsq326ybHrEhB7M'
       );
-      expect(addressType).toBe(AddressType.Byron);
+      expect(addressType).toBe(EraAddressType.Byron);
     });
 
     it('Should detect a valid Icarus address', () => {
-      const addressType = cardanoService.getAddressType('Ae2tdPwUPEZGvXJ3ebp4LDgBhbxekAH2oKZgfahKq896fehv8oCJxmGJgLt');
-      expect(addressType).toBe(AddressType.Byron);
+      const addressType = cardanoService.getEraAddressType('Ae2tdPwUPEZGvXJ3ebp4LDgBhbxekAH2oKZgfahKq896fehv8oCJxmGJgLt');
+      expect(addressType).toBe(EraAddressType.Byron);
     });
 
     it('Should return null if address is wrong', () => {
-      const addressType = cardanoService.getAddressType('WRONG');
+      const addressType = cardanoService.getEraAddressType('WRONG');
       expect(addressType).toBe(null);
     });
   });
