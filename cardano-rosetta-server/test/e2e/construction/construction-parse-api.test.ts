4cb7800ef157f29377d7043cad6b91462e812e9b
Tomás Bustamante <tbustamante@atixlabs.com>
2020-11-20 16:51:57 -0300

    feat: stake registration and deregistration parsing
    Refs #205

--- cardano-rosetta-server/test/e2e/construction/construction-parse-api.test.ts
+++ cardano-rosetta-server/test/e2e/construction/construction-parse-api.test.ts
@@ -5,8 +5,10 @@ import { FastifyInstance } from 'fastify';
 import { setupDatabase, setupServer } from '../utils/test-utils';
 import {
   CONSTRUCTION_INVALID_TRANSACTION,
-  CONSTRUCTION_PARSE_OPERATIONS,
+  constructionParseOperations,
   CONSTRUCTION_PAYLOADS_REQUEST,
+  CONSTRUCTION_PAYLOADS_WITH_STAKE_KEY_REGISTRATION,
+  CONSTRUCTION_UNSIGNED_TRANSACTION_WITH_STAKE_KEY_REGISTRATION_WITH_EXTRA_DATA,
   CONSTRUCTION_SIGNED_TRANSACTION_WITH_EXTRA_DATA,
   CONSTRUCTION_UNSIGNED_TRANSACTION_WITH_EXTRA_DATA
 } from '../fixture-data';
@@ -38,8 +40,8 @@ describe(CONSTRUCTION_PARSE_ENDPOINT, () => {
       payload: generateParsePayload('cardano', 'mainnet', true, CONSTRUCTION_SIGNED_TRANSACTION_WITH_EXTRA_DATA)
     });
     expect(response.statusCode).toEqual(StatusCodes.OK);
-    expect(response.json().operations).toEqual(CONSTRUCTION_PARSE_OPERATIONS);
-    expect(response.json().signers).toEqual([CONSTRUCTION_PAYLOADS_REQUEST.operations[0].account.address]);
+    expect(response.json().operations).toEqual(constructionParseOperations(CONSTRUCTION_PAYLOADS_REQUEST));
+    expect(response.json().signers).toEqual([CONSTRUCTION_PAYLOADS_REQUEST.operations[0].account!.address]);
   });
 
   test('Should return 1 input, 2 outputs and empty signers if a valid unsigned transaction is set', async () => {
@@ -50,7 +52,25 @@ describe(CONSTRUCTION_PARSE_ENDPOINT, () => {
     });
 
     expect(response.statusCode).toEqual(StatusCodes.OK);
-    expect(response.json().operations).toEqual(CONSTRUCTION_PARSE_OPERATIONS);
+    expect(response.json().operations).toEqual(constructionParseOperations(CONSTRUCTION_PAYLOADS_REQUEST));
+    expect(response.json().signers).toEqual([]);
+  });
+
+  test('Should return 1 input, 2 outputs, 1 stake key registration and empty signers if a valid unsigned transaction is set', async () => {
+    const response = await server.inject({
+      method: 'post',
+      url: CONSTRUCTION_PARSE_ENDPOINT,
+      payload: generateParsePayload(
+        'cardano',
+        'mainnet',
+        false,
+        CONSTRUCTION_UNSIGNED_TRANSACTION_WITH_STAKE_KEY_REGISTRATION_WITH_EXTRA_DATA
+      )
+    });
+    expect(response.statusCode).toEqual(StatusCodes.OK);
+    expect(response.json().operations).toEqual(
+      constructionParseOperations(CONSTRUCTION_PAYLOADS_WITH_STAKE_KEY_REGISTRATION)
+    );
     expect(response.json().signers).toEqual([]);
   });
 
