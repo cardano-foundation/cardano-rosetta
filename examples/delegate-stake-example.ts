17f0698f3411d672ae56d4a949ae79d7cfba6a25
Juan Cruz <jvieiro@atixlabs.com>
2020-12-15 13:15:01 -0300

    fix: delegate stake example

--- examples/delegate-stake-example.ts
+++ examples/delegate-stake-example.ts
@@ -16,14 +16,10 @@ import {
 } from "./commons";
 const logger = console;
 
-const PRIVATE_KEY =
-  "41d9523b87b9bd89a4d07c9b957ae68a7472d8145d7956a692df1a8ad91957a2c117d9dd874447f47306f50a650f1e08bf4bec2cfcb2af91660f23f2db912977";
 const SEND_FUNDS_ADDRESS =
-  "addr1qqr585tvlc7ylnqvz8pyqwauzrdu0mxag3m7q56grgmgu7sxu2hyfhlkwuxupa9d5085eunq2qywy7hvmvej456flknsug829n";
-const PRIVATE_STAKING_KEY =
-  "nqDmxvW0ytblMFnEbMFO8rVgl5V/pYnso/TPOyWz3vylms3kaRPDAg49S/rYyJh+eb+e0X3jeqCBhXgAHNjLCA==";
+  "addr_test1qqr585tvlc7ylnqvz8pyqwauzrdu0mxag3m7q56grgmgu7sxu2hyfhlkwuxupa9d5085eunq2qywy7hvmvej456flknswgndm3";
 const STAKE_POOL_KEY_HASH =
-  "22a8dc80b6fb4852150960c2e3896fa0a03498f514afc474c33152b6";
+  "6be215192dc01e5ca4cfba0959586f581a865bfccc2984478dad1657";
 
 const buildRegistrationOperation = (
   stakingKey: string,
@@ -60,50 +56,73 @@ const buildDelegationOperation = (
 });
 
 const doRun = async (): Promise<void> => {
-  const keys = generateKeys(PRIVATE_KEY);
+  const keyAddressMapper = {};
+  const paymentKeys = generateKeys();
   logger.info(
-    `[doRun] secretKey ${Buffer.from(keys.secretKey).toString("hex")}`
+    `[doRun] payment secretKey ${Buffer.from(paymentKeys.secretKey).toString(
+      "hex"
+    )}`
   );
-  const publicKey = Buffer.from(keys.publicKey).toString("hex");
+  const paymentPublicKey = Buffer.from(paymentKeys.publicKey).toString("hex");
 
-  const stakingKey = Buffer.from(
-    generateKeys(PRIVATE_STAKING_KEY).publicKey
-  ).toString("hex");
-  logger.info(`[doRun] secretKey ${stakingKey}`);
-  const address = await constructionDerive(publicKey);
+  const stakingKeys = generateKeys();
+  logger.info(
+    `[doRun] staking secretKey ${Buffer.from(stakingKeys.secretKey).toString(
+      "hex"
+    )}`
+  );
+  const stakingPublicKey = Buffer.from(stakingKeys.publicKey).toString("hex");
+  const stakeAddress = await constructionDerive(stakingPublicKey, "Reward");
+  logger.info(`[doRun] stake address is ${stakeAddress}`);
+  keyAddressMapper[stakeAddress] = stakingKeys;
+
+  const paymentAddress = await constructionDerive(
+    paymentPublicKey,
+    "Base",
+    stakingPublicKey
+  );
+
+  keyAddressMapper[paymentAddress] = paymentKeys;
   const unspents = await waitForBalanceToBe(
-    address,
+    paymentAddress,
     (response) => response.coins.length !== 0
   );
-  const builtOperations = buildOperation(unspents, address, SEND_FUNDS_ADDRESS);
-  const totalOperations = builtOperations.operations.length;
+  const builtOperations = buildOperation(
+    unspents,
+    paymentAddress,
+    SEND_FUNDS_ADDRESS
+  );
+  const currentIndex = builtOperations.operations.length - 1;
   const builtRegistrationOperation = buildRegistrationOperation(
-    stakingKey,
-    totalOperations
+    stakingPublicKey,
+    currentIndex
   );
   const builtDelegationOperation = buildDelegationOperation(
-    stakingKey,
-    totalOperations + 1,
+    stakingPublicKey,
+    currentIndex + 1,
     STAKE_POOL_KEY_HASH
   );
   builtOperations.operations.push(builtRegistrationOperation);
   builtOperations.operations.push(builtDelegationOperation);
-  const preprocess = await constructionPreprocess(builtOperations, 1000);
+  logger.info(`[doRun] operations to be sent are ${JSON.stringify(builtOperations.operations)}`);
+  const preprocess = await constructionPreprocess(
+    builtOperations.operations,
+    1000
+  );
   const metadata = await constructionMetadata(preprocess);
   const payloads = await constructionPayloads({
     operations: builtOperations.operations,
-    metadata,
+    metadata
   });
-  const signatures = signPayloads(payloads.payloads, keys);
+  const signatures = signPayloads(payloads.payloads, keyAddressMapper);
   const combined = await constructionCombine(
     payloads.unsigned_transaction,
     signatures
   );
   logger.info(`[doRun] signed transaction is ${combined.signed_transaction}`);
   const hashResponse = await constructionSubmit(combined.signed_transaction);
-  logger.info(
-    `[doRun] transaction with hash ${hashResponse.transaction_identifier.hash} sent`
-  );
+  const transactionHash = hashResponse.transaction_identifier.hash;
+  logger.info(`[doRun] transaction with hash ${transactionHash} sent`);
 };
 
 doRun()
