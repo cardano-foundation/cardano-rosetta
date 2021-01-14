6923a91a9a0d797172d3b3a1c8ad6c15e252d21f
Alan Verbner <alan.verbner@iohk.io>
2021-01-14 12:08:59 -0300

    feat: added multiassets representation to the openapi.json definition

--- cardano-rosetta-server/src/types/rosetta-types.d.ts
+++ cardano-rosetta-server/src/types/rosetta-types.d.ts
@@ -365,10 +365,15 @@ declare namespace Components {
        * Any additional information related to the currency itself. For example, it would be useful to populate this object with the contract address of an ERC-20 token.
        * example:
        * {
-       *   "Issuer": "Satoshi"
+       *   "policy": "deadbeef"
        * }
        */
-      metadata?: {};
+      metadata?: {
+        /**
+         * Policy Id hex string
+         */
+        policy?: string;
+      };
     }
     /**
      * CurveType is the type of cryptographic curve associated with a PublicKey. * secp256k1: SEC compressed - `33 bytes` (https://secg.org/sec1-v2.pdf#subsubsection.2.3.3) * secp256r1: SEC compressed - `33 bytes` (https://secg.org/sec1-v2.pdf#subsubsection.2.3.3) * edwards25519: `y (255-bits) || x-sign-bit (1-bit)` - `32 bytes` (https://ed25519.cr.yp.to/ed25519-20110926.pdf) * tweedle: 1st pk : Fq.t (32 bytes) || 2nd pk : Fq.t (32 bytes) (https://github.com/CodaProtocol/coda/blob/develop/rfcs/0038-rosetta-construction-api.md#marshal-keys)
@@ -572,11 +577,15 @@ declare namespace Components {
        */
       depositAmount?: /* Amount is some Value of a Currency. It is considered invalid to specify a Value without a Currency. */ Amount;
       /**
-       * If it's a deRegistration operation, the amount will re returned here.
+       * If it's a deregistration operation, the amount will re returned here.
        */
       fundAmount?: /* Amount is some Value of a Currency. It is considered invalid to specify a Value without a Currency. */ Amount;
       staking_credential?: /* PublicKey contains a public key byte array for a particular CurveType encoded in hex. Note that there is no PrivateKey struct as this is NEVER the concern of an implementation. */ PublicKey;
       pool_key_hash?: string;
+      /**
+       * All the multiassets for the given unspent
+       */
+      multiassets?: /* Amount is some Value of a Currency. It is considered invalid to specify a Value without a Currency. */ Amount[];
     }
     /**
      * OperationStatus is utilized to indicate which Operation status are considered successful.
