794594b7653f91e8065e86ebf4ba2218f3ad82c0
Juan Cruz <jvieiro@atixlabs.com>
2021-01-27 16:14:50 -0300

    feat: improve code of validations for multi assets

--- cardano-rosetta-server/src/server/utils/validations.ts
+++ cardano-rosetta-server/src/server/utils/validations.ts
@@ -1,5 +1,9 @@
 import { PUBLIC_KEY_BYTES_LENGTH, AddressType, CurveType, ASSET_NAME_LENGTH, POLICY_ID_LENGTH } from './constants';
 
+const tokenNameValidation = new RegExp(`^[0-9a-fA-F]{0,${ASSET_NAME_LENGTH}}$`);
+
+const policyIdValidation = new RegExp(`^[0-9a-fA-F]{${POLICY_ID_LENGTH}}$`);
+
 export const VALID_CURVE_TYPE = CurveType.edwards25519;
 
 export const isKeyValid = (publicKeyBytes: string, curveType: string): boolean =>
@@ -8,8 +12,6 @@ export const isKeyValid = (publicKeyBytes: string, curveType: string): boolean =
 export const isAddressTypeValid = (type: string): boolean =>
   [...Object.values(AddressType), '', undefined].includes(type);
 
-export const isTokenNameValid = (name: string): boolean =>
-  new RegExp(`^[0-9a-fA-F]{0,${ASSET_NAME_LENGTH}}$`).test(name);
+export const isTokenNameValid = (name: string): boolean => tokenNameValidation.test(name);
 
-export const isPolicyIdValid = (policyId: string): boolean =>
-  new RegExp(`^[0-9a-fA-F]{${POLICY_ID_LENGTH}}$`).test(policyId);
+export const isPolicyIdValid = (policyId: string): boolean => policyIdValidation.test(policyId);
