0d8fc77b985791487383f4fef4fe67bd01b8eba3
Juan Cruz <jvieiro@atixlabs.com>
2021-02-05 12:16:49 -0300

    test: adapt /network/options and /account/balance tests for rosetta v1.4.10

--- cardano-rosetta-server/test/e2e/network/network-options-api.test.ts
+++ cardano-rosetta-server/test/e2e/network/network-options-api.test.ts
@@ -132,11 +132,14 @@ const allow = {
       retriable: true
     }
   ],
-  historical_balance_lookup: true
+  historical_balance_lookup: true,
+  call_methods: [],
+  balance_exemptions: [],
+  mempool_coins: false
 };
 
 const version = {
-  rosetta_version: '1.4.4',
+  rosetta_version: '1.4.10',
   node_version: 'cardano-node 1.18.0 - linux-x86_64 - ghc-8.6\ngit rev 36ad7b90bfbde8afd41b68ed9b928df3fcab0dbc',
   middleware_version: packageJson.version,
   metadata: {}
