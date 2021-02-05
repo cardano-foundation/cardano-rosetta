b0c262084d7a405327dfad8fc42952313f3b0b6c
Juan Cruz <jvieiro@atixlabs.com>
2021-02-05 12:16:49 -0300

    test: adapt /network/options and /account/balance tests for rosetta v1.4.10

--- cardano-rosetta-server/test/e2e/account/account-api.test.ts
+++ cardano-rosetta-server/test/e2e/account/account-api.test.ts
@@ -80,13 +80,13 @@ describe('/account/balance endpoint', () => {
     expect(response.statusCode).toEqual(StatusCodes.OK);
     expect(response.json()).toEqual({
       balances: [{ currency: { decimals: 6, symbol: 'ADA' }, value: '21063' }],
-      block_identifier: latestBlockIdentifier,
-      coins: [
-        {
-          coin_identifier: { identifier: 'af0dd90debb1fbaf3854b90686ba2d6f7c95416080e8cda18d9ea3cb6bb195ad:0' },
-          amount: { value: '21063', currency: { symbol: 'ADA', decimals: 6 } }
-        }
-      ]
+      block_identifier: latestBlockIdentifier
+      // coins: [
+      //   {
+      //     coin_identifier: { identifier: 'af0dd90debb1fbaf3854b90686ba2d6f7c95416080e8cda18d9ea3cb6bb195ad:0' },
+      //     amount: { value: '21063', currency: { symbol: 'ADA', decimals: 6 } }
+      //   }
+      // ]
     });
   });
 
@@ -102,8 +102,8 @@ describe('/account/balance endpoint', () => {
       block_identifier: {
         hash: '7c6901c6346781c2bc5cbc49577490e336c2545c320ce4a61605bc71a9c5bed0',
         index: 20
-      },
-      coins: AE2HashAccountUtxos
+      }
+      // coins: AE2HashAccountUtxos
     });
   });
 
@@ -125,8 +125,8 @@ describe('/account/balance endpoint', () => {
       block_identifier: {
         hash: '7c6901c6346781c2bc5cbc49577490e336c2545c320ce4a61605bc71a9c5bed0',
         index: 20
-      },
-      coins: AE2HashAccountUtxos
+      }
+      // coins: AE2HashAccountUtxos
     });
   });
 
@@ -148,8 +148,8 @@ describe('/account/balance endpoint', () => {
       block_identifier: {
         hash: 'd3fdc8c8ea4050cc87a21cb73110d54e3ec92f8ae76941e8a1957ed6e6a7e0b0',
         index: 30
-      },
-      coins: AE2HashAccountUtxos
+      }
+      // coins: AE2HashAccountUtxos
     });
   });
 
@@ -190,21 +190,21 @@ describe('/account/balance endpoint', () => {
             symbol: 'ADA'
           }
         }
-      ],
-      coins: [
-        {
-          coin_identifier: {
-            identifier: '4bcf79c0c2967986749fd0ae03f5b54a712d51b35672a3d974707c060c4d8dac:1'
-          },
-          amount: { value: '10509579714', currency: { decimals: 6, symbol: 'ADA' } }
-        },
-        {
-          coin_identifier: {
-            identifier: 'bcc57134d1bd588b00f40142f0fdc17db5f35047e3196cdf26aa7319524c0014:1'
-          },
-          amount: { value: '999800000', currency: { decimals: 6, symbol: 'ADA' } }
-        }
       ]
+      // coins: [
+      //   {
+      //     coin_identifier: {
+      //       identifier: '4bcf79c0c2967986749fd0ae03f5b54a712d51b35672a3d974707c060c4d8dac:1'
+      //     },
+      //     amount: { value: '10509579714', currency: { decimals: 6, symbol: 'ADA' } }
+      //   },
+      //   {
+      //     coin_identifier: {
+      //       identifier: 'bcc57134d1bd588b00f40142f0fdc17db5f35047e3196cdf26aa7319524c0014:1'
+      //     },
+      //     amount: { value: '999800000', currency: { decimals: 6, symbol: 'ADA' } }
+      //   }
+      // ]
     });
   });
 
@@ -250,13 +250,13 @@ describe('/account/balance endpoint', () => {
             symbol: 'ADA'
           }
         }
-      ],
-      coins: [
-        {
-          amount: { value: '1000000', currency: { symbol: 'ADA', decimals: 6 } },
-          coin_identifier: { identifier: '6497b33b10fa2619c6efbd9f874ecd1c91badb10bf70850732aab45b90524d9e:0' }
-        }
       ]
+      // coins: [
+      //   {
+      //     amount: { value: '1000000', currency: { symbol: 'ADA', decimals: 6 } },
+      //     coin_identifier: { identifier: '6497b33b10fa2619c6efbd9f874ecd1c91badb10bf70850732aab45b90524d9e:0' }
+      //   }
+      // ]
     });
   });
   test('should only consider balance till last block and balance SHOULD be 0', async () => {
@@ -284,8 +284,8 @@ describe('/account/balance endpoint', () => {
             symbol: 'ADA'
           }
         }
-      ],
-      coins: []
+      ]
+      // coins: []
     });
   });
   test('should return 0 for the balance of stake account at block with no earned rewards', async () => {
@@ -392,8 +392,8 @@ describe('/account/balance endpoint', () => {
     address1vpfAccountBalances.forEach(accountBalance =>
       expect(response.json().balances).toContainEqual(accountBalance)
     );
-    expect(response.json().coins).toHaveLength(address1vpfCoins.length);
-    address1vpfCoins.forEach(address1vpfCoin => expect(response.json().coins).toContainEqual(address1vpfCoin));
+    // expect(response.json().coins).toHaveLength(address1vpfCoins.length);
+    // address1vpfCoins.forEach(address1vpfCoin => expect(response.json().coins).toContainEqual(address1vpfCoin));
   });
 
   // eslint-disable-next-line max-len
@@ -431,8 +431,8 @@ describe('/account/balance endpoint', () => {
     balancesAtBlock213891.forEach(accountBalance =>
       expect(responseAtBlock213891.json().balances).toContainEqual(accountBalance)
     );
-    expect(responseAtBlock213891.json().coins).toHaveLength(coinsAtBlock213891.length);
-    coinsAtBlock213891.forEach(coin => expect(responseAtBlock213891.json().coins).toContainEqual(coin));
+    // expect(responseAtBlock213891.json().coins).toHaveLength(coinsAtBlock213891.length);
+    // coinsAtBlock213891.forEach(coin => expect(responseAtBlock213891.json().coins).toContainEqual(coin));
     expect(responseAtBlock213892.statusCode).toEqual(StatusCodes.OK);
     expect(responseAtBlock213892.json().block_identifier).toEqual({
       index: 213892,
@@ -441,8 +441,8 @@ describe('/account/balance endpoint', () => {
     balancesAtBlock213892.forEach(accountBalance =>
       expect(responseAtBlock213892.json().balances).toContainEqual(accountBalance)
     );
-    expect(responseAtBlock213892.json().coins).toHaveLength(coinsAtBlock213892.length);
-    coinsAtBlock213892.forEach(coin => expect(responseAtBlock213892.json().coins).toContainEqual(coin));
+    // expect(responseAtBlock213892.json().coins).toHaveLength(coinsAtBlock213892.length);
+    // coinsAtBlock213892.forEach(coin => expect(responseAtBlock213892.json().coins).toContainEqual(coin));
   });
 
   test('should return balances for ma with empty name', async () => {
@@ -486,50 +486,50 @@ describe('/account/balance endpoint', () => {
             }
           }
         }
-      ],
-      coins: [
-        {
-          coin_identifier: { identifier: '02562c123f6d560e1250f5a46f7e95911b21fd8a9fa70157335c3a3d1d16bdda:0' },
-          amount: {
-            currency: { decimals: 6, symbol: 'ADA' },
-            value: '4800000'
-          },
-          metadata: {
-            '02562c123f6d560e1250f5a46f7e95911b21fd8a9fa70157335c3a3d1d16bdda:0': [
-              {
-                policyId: '181aace621eea2b6cb367adb5000d516fa785087bad20308c072517e',
-                tokens: [
-                  {
-                    value: '20',
-                    currency: {
-                      decimals: 0,
-                      symbol: '\\x',
-                      metadata: {
-                        policyId: '181aace621eea2b6cb367adb5000d516fa785087bad20308c072517e'
-                      }
-                    }
-                  }
-                ]
-              },
-              {
-                policyId: 'fc5a8a0aac159f035a147e5e2e3eb04fa3b5e67257c1b971647a717d',
-                tokens: [
-                  {
-                    value: '10',
-                    currency: {
-                      decimals: 0,
-                      symbol: '7376c3a57274',
-                      metadata: {
-                        policyId: 'fc5a8a0aac159f035a147e5e2e3eb04fa3b5e67257c1b971647a717d'
-                      }
-                    }
-                  }
-                ]
-              }
-            ]
-          }
-        }
       ]
+      // coins: [
+      //   {
+      //     coin_identifier: { identifier: '02562c123f6d560e1250f5a46f7e95911b21fd8a9fa70157335c3a3d1d16bdda:0' },
+      //     amount: {
+      //       currency: { decimals: 6, symbol: 'ADA' },
+      //       value: '4800000'
+      //     },
+      //     metadata: {
+      //       '02562c123f6d560e1250f5a46f7e95911b21fd8a9fa70157335c3a3d1d16bdda:0': [
+      //         {
+      //           policyId: '181aace621eea2b6cb367adb5000d516fa785087bad20308c072517e',
+      //           tokens: [
+      //             {
+      //               value: '20',
+      //               currency: {
+      //                 decimals: 0,
+      //                 symbol: '\\x',
+      //                 metadata: {
+      //                   policyId: '181aace621eea2b6cb367adb5000d516fa785087bad20308c072517e'
+      //                 }
+      //               }
+      //             }
+      //           ]
+      //         },
+      //         {
+      //           policyId: 'fc5a8a0aac159f035a147e5e2e3eb04fa3b5e67257c1b971647a717d',
+      //           tokens: [
+      //             {
+      //               value: '10',
+      //               currency: {
+      //                 decimals: 0,
+      //                 symbol: '7376c3a57274',
+      //                 metadata: {
+      //                   policyId: 'fc5a8a0aac159f035a147e5e2e3eb04fa3b5e67257c1b971647a717d'
+      //                 }
+      //               }
+      //             }
+      //           ]
+      //         }
+      //       ]
+      //     }
+      //   }
+      // ]
     });
   });
 });
