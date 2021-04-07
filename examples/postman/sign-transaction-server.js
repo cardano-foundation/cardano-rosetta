f0ce5397b37e5374cb893763949c366e3e3104c9
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2021-04-07 16:35:20 +1000

    chore: add simple validation to the sign-transaction server sign request body

--- examples/postman/sign-transaction-server.js
+++ examples/postman/sign-transaction-server.js
@@ -1,15 +1,20 @@
 import express from 'express';
+import { body, validationResult } from 'express-validator';
 import NaCl from 'tweetnacl';
 
 const app = express();
 app.use(express.json({ limit: "1mb" }));
 
-app.post("/sign", (req, res) => {
+app.post(
+  "/sign",
+  body(['private_key', 'public_key', 'hex_bytes']).isString().notEmpty(),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-    const private_key = req.body.private_key;
-    const public_key = req.body.public_key;
-    const hex_bytes = req.body.hex_bytes;
-
+    const { private_key, public_key, hex_bytes } = req.body;
     const key_pair = NaCl.sign.keyPair.fromSecretKey(Buffer.from(private_key+public_key, "hex"));
     const secret_key = key_pair.secretKey;
 
