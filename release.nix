23fd5b0a1a0af3fa43c2f6c9440b3cfd74fd4289
Robin Stumm <robin.stumm@iohk.io>
2022-11-22 16:46:31 +0100

    add std flake and tullia

--- release.nix
+++ release.nix
@@ -7,17 +7,18 @@
 #
 ############################################################################
 {
-  cardano-rosetta ? { rev = null; }
+  cardano-rosetta ? { rev = null; },
+  system ? builtins.currentSystem,
 }:
 
 let
   sources = import ./nix/sources.nix;
-  pkgs = import ./nix/pkgs.nix {};
+  pkgs = import ./nix/pkgs.nix { inherit system; };
 
 in
 
 pkgs.lib.fix (self: {
-  inherit ( import ./. {} ) cardano-rosetta-server;
+  inherit ( import ./. { inherit system; } ) cardano-rosetta-server;
   build-version = pkgs.writeText "version.json" (builtins.toJSON { inherit (cardano-rosetta) rev; });
   required = pkgs.releaseTools.aggregate {
     name = "required";
