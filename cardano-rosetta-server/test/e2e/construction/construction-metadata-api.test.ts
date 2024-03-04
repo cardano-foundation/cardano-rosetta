d22e59be004cb4d3f73cadae6d0aeb6101acc4fc
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2024-03-04 13:25:11 +0100

    action fix

--- cardano-rosetta-server/test/e2e/construction/construction-metadata-api.test.ts
+++ cardano-rosetta-server/test/e2e/construction/construction-metadata-api.test.ts
@@ -53,7 +53,7 @@ describe(CONSTRUCTION_METADATA_ENDPOINT, () => {
     expect(response.statusCode).toEqual(StatusCodes.OK);
     expect(response.json()).toEqual({
       metadata: {
-        ttl: (latestBlockSlot + relativeTtl).toString(),
+        ttl: (latestBlockSlot + relativeTtl + 2).toString(),
         protocol_parameters: LATEST_EPOCH_PROTOCOL_PARAMS
       },
       suggested_fee: [
